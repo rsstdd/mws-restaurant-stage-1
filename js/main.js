22d70be902952600df90a0e064f6707486f4fbac
rsstdd <rosstod@gmail.com>
2018-06-10 21:51:44 -0700

    Finish grid and style homepage

--- js/main.js
+++ js/main.js
@@ -128,7 +128,6 @@ resetRestaurants = restaurants => {
   }
   // Remove all map markers
   self.markers.forEach(m => {
-    console.log('self.markers.forEach', m);
     m.setMap(null);
   });
   self.markers = [];
@@ -151,28 +150,33 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 const createRestaurantHTML = restaurant => {
   const li = document.createElement('li');
+  li.className = 'col-xs-12 col-sm-6 col-md-3 col-lg-3';
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const text = document.createElement('div');
+  text.className = 'restaurant-text';
+  li.append(text);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  text.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  text.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  text.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more);
+  text.append(more);
 
   return li;
 };
